2910de3e6f1f0efc864f1090a3ede775f3275c6b
Antonio Junior <antoniocarlosjuninhojuca@gmail.com>
2022-05-23 18:26:59 -0300

    Exercicio - 2

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+double euro = 5.13;
+double real = 0;
+double quant = 0;
+
+Console.WriteLine("Insira o valor do euro: ");
+quant = Convert.ToInt32(Console.ReadLine());
+
+
+real = (quant * euro);
+Console.WriteLine($"O valor convertido em real é: {real}");
+
